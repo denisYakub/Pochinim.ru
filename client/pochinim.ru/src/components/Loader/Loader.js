2e41564d84fcf0d55bb460b83be4b2db193a080d
Denis Yakubov Laptop <yakybov_denis@icloud.com>
2024-04-12 14:21:26 +0300

    loader added

---
+++ client/pochinim.ru/src/components/Loader/Loader.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import '../Loader/Loader.css';
+import loadImg from '../../img/load.png';
+
+const Loader = ({showLoader, setShowLoader, timer}) => {
+    useEffect(() => {
+        setTimeout(() => {
+            setShowLoader(false);
+        }, timer);
+    }, []);
+
+    return(<div>
+            {showLoader?
+                <div className="loader">
+                <motion.img src={loadImg} alt="It should be load image..." className="loader-img"
+                    animate={{rotate: 360}}
+                    transition={{repeat: Infinity, duration: 3, type: "tween"}}></motion.img>
+                </div>
+            :
+                <></>}
+         </div>);
+}
+
+export default Loader;
--- client/pochinim.ru/src/components/SignUpAsMaster/phases/PasswordEnter.js
+++ client/pochinim.ru/src/components/SignUpAsMaster/phases/PasswordEnter.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import Loader from "../../Loader/Loader";
+
 const MasterPasswordEnter = ({password, setPassword}) => {
+    
+    const [showLoader, setShowLoader] = useState(false);
+
     return(<div className="phaseBlock">
-        passw
+        <button onClick={() => setShowLoader(true)}>Загрузить</button>
+        <Loader showLoader={showLoader} setShowLoader={setShowLoader} timer={3000}></Loader>
     </div>);
 };
 
